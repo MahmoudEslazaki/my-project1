584cdaad694f12e265c632d2dbb5865c78ed5440
MahmoudEslazaki <mahmoudeslam1055@gmail.com>
2025-07-22 21:44:38 +0300

    Add files via upload

--- student.java
+++ student.java
@@ -1,24 +1,48 @@
 public class student {
-    int age;
-    String name;
-    double gpa;
-    public void displayinfo(){
-        System.out.println("name" +name);
-        System.out.println("gpa"+gpa);
-        System.out.println("age"+age);
+    private String name;
+    private int age;
+    private double gpa;
 
-    public void study(){
-        System.out.println(name+ "is new study ");
+    public student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public void displayinfo() {
+        System.out.println("Name" + name);
+        System.out.println("Age" + age);
+        System.out.println("Gpa" + gpa);
+    }
+
+    public void study() {
+        System.out.println(name + "is study now .");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public double getGpa(){
+        return gpa;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
     }
     public static void main(String[]args){
-        student s1=new student();
-        s1.name="mahmoud";
-        s1.age=20;
-        s1.gpa=3.2;
-        student s2=new student();
-        s2.name="ahmed";
-        s2.age=21;
-        s2.gpa=2.5;
+        student s1 =new student("mahmoud",25,3.5);
+        student s2 =new student("mohamed",20,3.2);
         s1.displayinfo();
         s1.study();
         System.out.println();
